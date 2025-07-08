a56a7484be42097fec3378be39e1bd4c35e1a416
richmond-andoh <richmondandoh00@gmail.com>
2025-07-08 02:29:28 +0000

    beginner course

---
+++ Exception_lec/ExceptionLecture/Program.cs
@@ -0,0 +1,25 @@
+﻿// Create an Exception class that inherits from System.Exception
+
+namespace ExceptionLecture
+{
+    internal class Program
+    {
+        static void Main(string[], args) {
+            int repeat = 1;
+            int response = 1;
+
+            while (response == 1)
+            {
+                try
+                {
+                    Console.WriteLine($"Hello {repeat}");
+                }
+                catch (System.Exception)
+                {
+                    
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ Hello_World/Program.cs
@@ -0,0 +1,15 @@
+﻿// introduction to strings
+
+// first and lastname variables
+string firstName = "Richmond";
+string lastName = "Andoh";
+
+Console.WriteLine($"Hello My first Name is {firstName} and My surname is {lastName}!");
+
+
+//friends
+string firend1 = "Desmond";
+string friend2 = "Kojo Spencer";
+string friend3 = "Kwame";
+
+Console.WriteLine($"My friends are {firend1}, {friend2} and {friend3}!");
